fa4f472b042e103df26b58a81ef130a33911025e
艸先生 <kauia96@gmail.com>
2024-02-06 02:37:31 +0800

    feat:本地本地databass設定完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,50 @@
 class PostsController < ApplicationController
+  before_action :find_posts, only: %i[show edit update]
+
   def index
+    @posts = Post.order(id: :desc)
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: '文章建立成功'
+    else
+      render :new
+    end
+  end
+  
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to root_path, notice: '文章編輯成功'
+    else
+      render :edit
+    end
   end
 
-  def create
+  def destroy
+    @post.destroy
+    redirect_to root_path, notice: '文章刪除成功'
   end
+
+  private
+
+  def find_posts
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :subtitle, :body)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  acts_as_paranoid
+  
+  belongs_to :category
+
+  validates :title, presence: true
+  validates :subtitle, presence: true
+  validates :body, presence: true
+  
 end
