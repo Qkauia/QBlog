0adfcc760484d93983b4f43a073c3b1d994c434d
艸先生 <kauia96@gmail.com>
2024-02-16 23:48:53 +0800

    feat:添加滿版預覽markdown

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -1,11 +1,31 @@
 // app/javascript/controllers/preview_controller.js
 import { Controller } from "@hotwired/stimulus";
 import { marked } from "marked";
+import Swal from "sweetalert2";
 
 export default class extends Controller {
   static targets = ["source", "preview"];
 
+  connect() {
+    this.show(); // 表單右側即時顯示
+  }
+
   show() {
-    this.previewTarget.innerHTML = marked(this.sourceTarget.value);
+    const markdown = this.sourceTarget.value;
+    const html = marked(markdown);
+    this.previewTarget.innerHTML = html;
+  }
+
+  showInModal() {
+    const markdown = this.sourceTarget.value;
+    const htmlContent =
+      '<div style="text-align: left;">' + marked(markdown) + "</div>";
+
+    Swal.fire({
+      title: "(ʘ_ʘ)",
+      html: htmlContent,
+      width: "90%",
+      showCloseButton: true,
+    });
   }
 }
